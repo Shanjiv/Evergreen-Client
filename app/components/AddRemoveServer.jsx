fff4321198fefa37664c75c9d3277886bef7aca5
landguy423 <landguy423@gmail.com>
2017-04-30 00:50:50 +0800

    fix dropdown

--- app/components/AddRemoveServer.jsx
+++ app/components/AddRemoveServer.jsx
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
 
 class AddRemoveServer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            addFlag: false,
+            removeFlag: false
+        };
+    }
+
+    showAddDropdown() {
+        let {addFlag} = this.state;
+        this.setState({addFlag: !addFlag});
+    }
+
+    showRemoveDropdown() {
+        let {removeFlag} = this.state;
+        this.setState({removeFlag: !removeFlag});
+    }
+
     render() {
         return (
             <div>
-                <button className="buttonAdd" type="button" data-toggle="example-dropdown">Add Server</button>
-                <div className="dropdown-pane" id="example-dropdown" data-dropdown data-auto-focus="true">
+                <button className="buttonAdd" type="button" data-toggle="example-dropdown" onClick={this.showAddDropdown.bind(this)} >Add Server</button>
+                { this.state.addFlag == true ?
+                <div className="dropdown-pane-new " id="example-dropdown" data-dropdown data-auto-focus="true">
                     Add server
                     <div className="row">
                         <div className="medium-6 columns">
@@ -25,8 +45,11 @@ class AddRemoveServer extends Component {
                         </div>
                     </div>
                 </div>
-                <button className="buttonRemove" type="button" data-toggle="example-dropdown">Remove Server</button>
-                <div className="dropdown-pane" id="example-dropdown" data-dropdown data-auto-focus="true">
+                : null
+                }
+                <button className="buttonRemove" type="button" data-toggle="example-dropdown" onClick={this.showRemoveDropdown.bind(this)}>Remove Server</button>
+                { this.state.removeFlag == true ?
+                <div className="dropdown-pane-new {this.state.removeFlag ? show-dropdown : null}" id="example-dropdown" data-dropdown data-auto-focus="true">
                     Add server
                     <div className="row">
                         <div className="medium-6 columns">
@@ -41,6 +64,8 @@ class AddRemoveServer extends Component {
                         </label>
                     </div>
                 </div>
+                : null
+                }
             </div>
         );
     }
