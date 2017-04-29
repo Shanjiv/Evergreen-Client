650476b554e1bb8772b00c048c70a59b52a0caa0
Shanseevan Kanagaratnam <shanjiv@hotmail.com>
2017-04-29 12:10:14 +0200

    Added flexbox

--- app/components/AddRemoveServer.jsx
+++ app/components/AddRemoveServer.jsx
@@ -3,19 +3,19 @@ import React, {Component} from 'react';
 class AddRemoveServer extends Component {
     render() {
         return (
-            <div>
-                <button className="buttonAdd" type="button" data-toggle="example-dropdown">Add Server</button>
+            <div className="AddRemoveServer">
+                <button className="button" type="button" data-toggle="example-dropdown">Add Server</button>
                 <div className="dropdown-pane" id="example-dropdown" data-dropdown data-auto-focus="true">
-                    Add server
+                    Add Server
                     <div className="row">
                         <div className="medium-6 columns">
-                            <label>Server
+                            <label>Servername
                                 <input type="text" placeholder="Servername"/>
                             </label>
                         </div>
                         <div className="medium-6 columns">
-                            <label>IP
-                                <input type="text" placeholder="IP-adress"/>
+                            <label>Ip-Adress
+                                <input type="text" placeholder="IP"/>
                             </label>
                         </div>
                         <div className="medium-6 columns">
@@ -25,20 +25,15 @@ class AddRemoveServer extends Component {
                         </div>
                     </div>
                 </div>
-                <button className="buttonRemove" type="button" data-toggle="example-dropdown">Remove Server</button>
-                <div className="dropdown-pane" id="example-dropdown" data-dropdown data-auto-focus="true">
-                    Add server
+                <button className="button" type="button" data-toggle="example-dropdown-1">Remove Server</button>
+                <div className="dropdown-pane" id="example-dropdown-1" data-dropdown data-auto-focus="true">
+                    Remove Server
                     <div className="row">
                         <div className="medium-6 columns">
-                            <label>Server
+                            <label>Servername
                                 <input type="text" placeholder="Servername"/>
                             </label>
                         </div>
-                    </div>
-                    <div className="medium-6 columns">
-                        <label>Port
-                            <input type="text" placeholder="Port"/>
-                        </label>
                     </div>
                 </div>
             </div>
--- app/components/ServerBox.jsx
+++ app/components/ServerBox.jsx
@@ -5,16 +5,16 @@ class ServerBox extends Component {
         return (
             <div>
                 <div className="row">
-                    <label>Select Server
-                        <div className="larger-12 columns">
+                    <div className="larger-12 columns">
+                        <label>Select your Server
                             <select>
                                 <option value="husker">Husker</option>
                                 <option value="starbuck">Starbuck</option>
                                 <option value="hotdog">Hot Dog</option>
                                 <option value="apollo">Apollo</option>
                             </select>
-                        </div>
-                    </label>
+                        </label>
+                    </div>
                 </div>
             </div>
         );
